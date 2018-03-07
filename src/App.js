615a849bf29516497192515da155e9c1dd95cc34
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-03-07 14:59:32 +0100

    Handling events with methods

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ class App extends Component {
       { name: 'Yudha', age: 27 },
       { name: 'Rika', age: 36 },
       { name: 'Zulfiqar', age: 34 }
-      
-
     ]
+  }
+
+  switchNamehandler = () => {
+    console.log('Was clicked!');
   }
 
   render() {
@@ -18,7 +20,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p> This is really working </p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNamehandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My Hobbies: Racing </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
